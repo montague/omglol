bf1a218329ab84d5085248765f953eddf24984f7
ian asaff <ian.asaff@gmail.com>
2013-11-16 08:48:37 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omglol::Application.config.secret_key_base = '9f856f5aefcdc38bae0777cd54d8ce57a3b58ab472be1d1e1da27b6f3a843b16f6a885b24105671e4eb9f679b2b6de927f2e5dffcd355a8a9d44c121d4a5ffe4'
