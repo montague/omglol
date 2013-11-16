ea1409a3e1ae1c2208e3e54c92368c25a6d40a4c
ian asaff <ian.asaff@gmail.com>
2013-11-16 12:53:21 -0500

    fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Omglol::Application.routes.draw do
-  root to: 'pages/index'
+  root to: 'pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
